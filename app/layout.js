b19696a22ec18bf922bec948c785f26ae197e3a7
mrwindupbirdesq <nmaahs11@gmail.com>
2024-09-18 10:46:26 -0700

    Add and style background image

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css";
 import Link from "next/link";
+import Image from "next/image";
+import background from "./topo_tmp.webp"
 
 export const metadata = {
   title: "Technical Writer — Dev Docs / APIs — Nick Maahs",
@@ -10,6 +12,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <div className='bgWrap'>
+          <Image
+            alt="Topographical map"
+            src={background}
+            placeholder="blur"
+            quality={100}
+            fill
+            sizes="100vw"
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </div>
         <header>
           <nav>
             <Link href="/">
