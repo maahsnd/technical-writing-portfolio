312ef9dd40b08b232409df9654c2f4c895ea0f11
mrwindupbirdesq <nmaahs11@gmail.com>
2024-09-19 14:38:37 -0700

    Align development, remove content

--- app/development/page.js
+++ app/development/page.js
@@ -3,7 +3,6 @@ import styles from "./page.module.css";
 export default function DevHome() {
     return (
         <div className={styles.container}>
-            <p>I like to keep things simple. To view my development work, please visit my GitHub: </p>
             <Link href={'https://github.com/maahsnd'} target="_blank" rel="noopener noreferrer">
                 <img
                     src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
