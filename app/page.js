06d41c2a062a831466281789f1c51868702a4a57
mrwindupbirdesq <nmaahs11@gmail.com>
2024-09-18 20:25:13 -0700

    Decrease ul margin

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 import PostCard from "@/components/postCard/PostCard";
 import { getPostMetadata } from "@/utils/getPostMetadata";
 import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function Home() {
   const postMeta = getPostMetadata('portfolio');
   return (
     <main>
       <div>
-        <ul>
+        <ul className={styles.homeLinkList}>
           <li>
             <Link href={`/portfolio`}>
               <h3>Technical Writing</h3>
