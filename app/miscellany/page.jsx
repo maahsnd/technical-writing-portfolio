6d8d649cd55b2284b6daf18d427f35050cd78a36
mrwindupbirdesq <nmaahs11@gmail.com>
2024-09-19 14:43:25 -0700

    Align miscellany

--- app/miscellany/page.jsx
+++ app/miscellany/page.jsx
@@ -1,6 +1,8 @@
+import styles from './page.module.css'
+
 export default function Miscellany() {
     return (
-        <div>
+        <div className={styles.container}>
             No miscellany yet
         </div>
     )
